81a3e74edcc48474c0ec929d5ed3331552b21ea6
Yeltsin Batiancila <yeltsinpb@gmail.com>
2018-07-28 17:44:05 +0800

    added files

---
+++ app/src/main/java/com/batiancila/a03_laboratory_exercise_1/DisplayMessageActivity.java
@@ -0,0 +1,21 @@
+package com.batiancila.a03_laboratory_exercise_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+    }
+}
---
+++ app/src/main/java/com/batiancila/a03_laboratory_exercise_1/MyActivity.java
@@ -0,0 +1,26 @@
+package com.batiancila.a03_laboratory_exercise_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.batiancila.a03_laboratory_exercise_1.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this , DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
